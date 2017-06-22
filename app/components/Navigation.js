35cd2acfb8235c5fb88a6f479c5a4a85c0f260a7
Alexey Chernenko <alexey.chernenko@yalantis.com>
2017-06-22 16:34:19 +0300

    Revert "init commit"
    This reverts commit 566883a90f7f1cbbb52a029d335136c4a749f9b0.

---
+++ app/components/Navigation.js
@@ -0,0 +1,35 @@
+/**
+ * Created by saionara1 on 6/21/17.
+ */
+import React, {Component} from "react";
+import {connect, Provider} from "react-redux";
+import {BackAndroidi} from "react-native";
+import configureStore from "../store/configureStore.js";
+import {StackNavigator} from "react-navigation";
+import Login from "./Login";
+
+const store = configureStore();
+const Routes = {Login: {screen: Login},};
+const Navigator = StackNavigator(Routes, {});
+
+export class Navigation extends Component {
+
+    render() {
+        return (
+            <Provider store={store}>
+                <Navigator/>
+            </Provider>
+        );
+    }
+
+}
+
+function mapStateToProps(state) {
+    return {}
+}
+function mapDispatchToProps(dispatch) {
+    return {}
+}
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps)(Navigation);
---
+++ app/resources/dimens.js
@@ -0,0 +1,4 @@
+/**
+ * Created by saionara1 on 6/21/17.
+ */
+export default {}
