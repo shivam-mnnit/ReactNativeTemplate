a91c7bec9a929e0c9b642cc4866ade7560e79098
Alexey Chernenko <alexey.chernenko@yalantis.com>
2017-06-23 12:08:05 +0300

    started implamenting login ui

--- app/components/Login.js
+++ app/components/Login.js
@@ -2,11 +2,84 @@
  * Created by saionara1 on 6/21/17.
  */
 import React, {Component} from "react";
-import {View} from "react-native";
+import {StatusBar, Text, View,Image} from "react-native";
+import {Button, Container, Content} from "native-base";
+import colors from "../resources/colors";
+import ValidationTextInput from "./ValidationTextInput";
+import consts from "../const";
+import dimens from "../resources/dimens";
 
 export default class Login extends Component {
+    static navigationOptions = {
+        header: null
+    };
 
+    //noinspection JSMethodCanBeStatic
     render() {
-        return (<View />);
+        return (
+            <Container style={loginStyles.containerStyle}>
+                <StatusBar style={loginStyles.statusBarStyle}/>
+                <Content contentContainerStyle={loginStyles.contentStyle}>
+                    <ValidationTextInput
+                        validate={(text) => this.validateEmail(text)}
+                        label={'Email'}
+                        style={loginStyles.emailStyle}
+                        color={colors.accentColor}/>
+                    <ValidationTextInput
+                        validate={(text) => this.validatePassword(text)}
+                        label={'Password'}
+                        style={loginStyles.emailStyle}
+                        color={colors.accentColor}/>
+
+                    <Button
+                        style={loginStyles.buttonStyle}>
+                        <Text style={loginStyles.buttonTextStyle}>Sign In</Text>
+                    </Button>
+                </Content>
+            </Container>);
+    }
+
+    validateEmail(text) {
+        return consts.EMAIL_REGEX.test(text);
+    }
+
+    validatePassword(text) {
+        if (text < consts.MIN_PASSWORD_LENGTH) {
+            return false;
+        }
+        return true;
     }
 }
+
+
+const loginStyles = {
+    containerStyle: {
+        flexDirection: 'row',
+        backgroundColor: colors.primaryColor,
+        alignItems: 'center'
+    },
+    contentStyle: {
+        flex: 0,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        alignSelf: 'stretch',
+        marginHorizontal: dimens.margin_large
+    },
+    statusBarStyle: {
+        backgroundColor: colors.primaryColor
+    },
+    emailStyle: {
+        alignSelf: 'stretch',
+    },
+    buttonStyle: {
+        marginTop: dimens.margin_medium,
+        alignSelf: 'stretch',
+        justifyContent: 'center',
+        backgroundColor: colors.accentColor
+    },
+    buttonTextStyle: {
+        color: 'white',
+        fontSize: dimens.text_size_button
+    }
+};
---
+++ app/components/ValidationTextInput.js
@@ -0,0 +1,74 @@
+import React, {Component} from "react";
+import {Content, Input, Item, Label, Text} from "native-base";
+
+
+export default class ValidationTextInput extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: ""
+        };
+    }
+
+    render() {
+        return (
+            <Content
+                shouldRasterizeIOS
+                renderToHardwareTextureAndroid
+                style={this.props.style} scrollEnabled={false}>
+                <Item floatingLabel style={{
+                    borderColor: this.state.error ? 'red' : this.props.color,
+                    ...validationTextStyles.itemStyle
+                }}>
+                    <Label ellipsizeMode='tail' numberOfLines={1}
+                           style={{color: this.props.color}}>{this.props.label}</Label>
+                    <Input
+                        style={validationTextStyles.inputStyle}
+                        secureTextEntry={this.props.secureTextEntry}
+                        onChangeText={(text) => this.handleTextChange(text)}
+                        value={this.props.value !== null ? this.props.value : ''}
+                        keyboardType={this.props.type !== null ? this.props.type : 'default'}
+                        onEndEditing={(event) => this.setError(event)}/>
+                </Item>
+                <Text style={validationTextStyles.errorTextStyle}>{this.state.error } </Text>
+            </Content>
+        );
+    }
+
+    handleTextChange(text) {
+        this.setState({error: ""});
+        if (this.props.onChangeText) {
+            this.props.onChangeText(text);
+        }
+        if (this.props.validateWhileEdit) {
+            this.setError();
+        }
+    }
+
+    setError(event) {
+        if (!this.props.validate(event.nativeEvent.text)) {
+            if (!event.nativeEvent.text || event.nativeEvent.text === "") {
+                this.setState({error: "Cannot be empty"});
+            } else {
+                this.setState({error: this.props.error});
+            }
+        } else {
+            this.setState({error: " "});
+        }
+    }
+}
+
+const validationTextStyles = {
+    errorTextStyle: {
+        color: 'red'
+    },
+    itemStyle: {
+        flex:0,
+        marginTop: 16
+    },
+    inputStyle: {
+        color: 'white'
+    }
+};
+
