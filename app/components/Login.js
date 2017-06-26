b9a1319482edccd8bad27f5556dd676da524f87b
Alexey Chernenko <alexey.chernenko@yalantis.com>
2017-06-26 11:23:48 +0300

    changed formatting settings

--- app/components/Login.js
+++ app/components/Login.js
@@ -6,7 +6,7 @@ import {View} from "react-native";
 
 export default class Login extends Component {
 
-    render() {
-        return (<View />);
-    }
+  render() {
+    return (<View />);
+  }
 }
