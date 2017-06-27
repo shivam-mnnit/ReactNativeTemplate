36ab8be906182f198d54268f5661d9d76fbd5054
Alexey Chernenko <alexey.chernenko@yalantis.com>
2017-06-27 20:12:21 +0300

    made up error handling and progress

--- app/components/Login.js
+++ app/components/Login.js
@@ -2,18 +2,38 @@
  * Created by saionara1 on 6/21/17.
  */
 import React, {Component} from "react";
-import {Image, StatusBar, Text} from "react-native";
-import {ActivityIndicator, Button, Container, Content} from "native-base";
+import {ActivityIndicator, Image, StatusBar, Text} from "react-native";
+import {Button, Container, Content} from "native-base";
 import colors from "../resources/colors";
 import ValidationTextInput from "./ValidationTextInput";
+import {connect} from "react-redux";
 import consts from "../const";
 import dimens from "../resources/dimens";
 import strings from "../resources/strings";
+import * as actions from "../actions/action-types";
+var Toast = require('react-native-toast');
 
-export default class Login extends Component {
+export class Login extends Component {
   static navigationOptions = {
     header: null
   };
+
+  constructor() {
+    super();
+    this.password = "";
+    this.email = "";
+  }
+
+  componentDidMount() {
+    this.props.dispatch({type: actions.PROGRESS, progress: false})
+  }
+
+  componentDidUpdate() {
+    if (this.props.login.loginError.message) {
+      Toast.showShortBottom(this.props.login.loginError.message);
+      this.props.dispatch({type: actions.LOGIN_ERROR, error: {}})
+    }
+  }
 
   //noinspection JSMethodCanBeStatic
   render() {
@@ -25,10 +45,12 @@ export default class Login extends Component {
           <ValidationTextInput
             validate={(text) => this.validateEmail(text)}
             label={strings.github_email}
+            onChangeText={(text) => this.email = text}
             style={loginStyles.emailStyle}
             color={colors.accentColor}/>
           <ValidationTextInput
             validate={(text) => this.validatePassword(text)}
+            onChangeText={(text) => this.password = text}
             label={strings.password}
             style={loginStyles.emailStyle}
             color={colors.accentColor}/>
@@ -38,8 +60,21 @@ export default class Login extends Component {
             onPress={() => this.onLoginPress()}>
             <Text style={loginStyles.buttonTextStyle}>{strings.sign_in}</Text>
           </Button>
+          {this.renderProgress()}
         </Content>
       </Container>);
+  }
+
+  renderProgress() {
+    if (this.props.login.progress) {
+      return ( <ActivityIndicator
+        color={colors.accentColor}
+        animating={true}
+        size={46}
+        style={loginStyles.progressStyle}/>)
+    } else {
+      return null;
+    }
   }
 
   validateEmail(text) {
@@ -52,44 +87,54 @@ export default class Login extends Component {
   }
 
   onLoginPress() {
-
+    this.props.dispatch({type: actions.LOGIN_ACTION, username: this.email, password: this.password})
   }
 }
 
 
 const loginStyles = {
-    containerStyle: {
-      flexDirection: 'row',
-      backgroundColor: colors.primaryColor,
-      alignItems: 'center'
-    },
-    contentStyle: {
-      flex: 0,
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center',
-      alignSelf: 'stretch',
-      marginHorizontal: dimens.margin_large
-    },
-    statusBarStyle: {
-      backgroundColor: colors.primaryColor
-    },
-    emailStyle: {
-      alignSelf: 'stretch',
-    },
-    buttonStyle: {
-      marginTop: dimens.margin_medium,
-      alignSelf: 'stretch',
-      justifyContent: 'center',
-      backgroundColor: colors.accentColor
-    },
-    buttonTextStyle: {
-      color: 'white',
-      fontSize: dimens.text_size_button
-    },
-    imageStyle: {
-      width: 150,
-      height: 150,
-    }
+  containerStyle: {
+    flexDirection: 'row',
+    backgroundColor: colors.primaryColor,
+    alignItems: 'center'
+  },
+  contentStyle: {
+    flex: 0,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'stretch',
+    marginHorizontal: dimens.margin_large
+  },
+  statusBarStyle: {
+    backgroundColor: colors.primaryColor
+  },
+  emailStyle: {
+    alignSelf: 'stretch',
+  },
+  buttonStyle: {
+    marginTop: dimens.margin_medium,
+    alignSelf: 'stretch',
+    justifyContent: 'center',
+    backgroundColor: colors.accentColor
+  },
+  buttonTextStyle: {
+    color: 'white',
+    fontSize: dimens.text_size_button
+  },
+  imageStyle: {
+    width: 150,
+    height: 150,
+  },
+  progressStyle: {
+    alignSelf: 'center',
+    position: 'absolute',
   }
-;
+};
+
+function mapStateToProps(state) {
+  console.log(state);
+  return {login: state.login}
+}
+
+export default connect(mapStateToProps)(Login)
--- app/reducers/loginReducer.js
+++ app/reducers/loginReducer.js
@@ -1,9 +1,39 @@
 /**
  * Created by saionara1 on 6/21/17.
  */
-const initialState = {};
+import * as actions from "../actions/action-types";
+
+const initialState = {
+  isLoggedIn: false,
+  user: {},
+  loginError: {},
+  progress: false
+};
 export default function loginReducer(state = initialState, action = {}) {
   switch (action.type) {
+    case actions.PROGRESS:
+      return {
+        ...state,
+        progress: action.progress
+      };
+    case actions.LOGIN_ERROR:
+      console.log('Catch error');
+      console.log(action);
+      return {
+        ...state,
+        isLoggedIn: false,
+        progress: false,
+        loginError: action.error
+      };
+    case actions.LOGIN_SUCCESS: {
+      console.log("Login Sucess");
+      return {
+        ...state,
+        progress: false,
+        isLoggedIn: true,
+        user: action.user
+      };
+    }
     default:
       return state;
   }
