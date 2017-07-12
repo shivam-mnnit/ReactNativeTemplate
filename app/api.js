2b3cd26b7610dec1f1ea53e094cf2c839422468f
Alexey Chernenko <alexey.chernenko@yalantis.com>
2017-07-12 11:43:16 +0300

    added const

--- app/api.js
+++ app/api.js
@@ -8,66 +8,66 @@ import queryString from "query-string";
 // work with api goes here
 
 export function getRepositories(token, page, limit) {
-  const params = queryString.stringify({
-    access_token: token,
-    page: page,
-    per_page: limit
-  });
-  return fetch('https://api.github.com/user/repos?' + params, {
-    method: 'GET',
-    headers: consts.BASE_HEADER
-  }).then((list) => {
-    return list.json()
-  })
-    .then((responseJson) => {
-      return responseJson;
-    })
-    .catch((error) => {
-      console.log(error);
+    const params = queryString.stringify({
+        access_token: token,
+        page: page,
+        per_page: limit
     });
+    return fetch('https://api.github.com/user/repos?' + params, {
+        method: 'GET',
+        headers: consts.BASE_HEADER
+    }).then((list) => {
+        return list.json()
+    })
+        .then((responseJson) => {
+            return responseJson;
+        })
+        .catch((error) => {
+            console.log(error);
+        });
 }
 
 
 export function getReadMe(token, username, repository) {
-  const params = queryString.stringify({
-    access_token: token,
-  });
-  return fetch('https://api.github.com/repos/' + username + '/' + repository + '/readme?'+params, {
-    method: 'GET',
-    headers: {
-      'Accept': 'application/vnd.github.v3.full+json',
-      'Content-Type': 'application/json',
-    }
-  }).then((readMe) => {
-    return readMe.json()
-  })
-    .then((responseJson) => {
-      return responseJson;
-    })
-    .catch((error) => {
-      console.log(error);
+    const params = queryString.stringify({
+        access_token: token,
     });
+    return fetch('https://api.github.com/repos/' + username + '/' + repository + '/readme?' + params, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/vnd.github.v3.full+json',
+            'Content-Type': 'application/json',
+        }
+    }).then((readMe) => {
+        return readMe.json()
+    })
+        .then((responseJson) => {
+            return responseJson;
+        })
+        .catch((error) => {
+            console.log(error);
+        });
 }
 
 
 export function getAccessToken(username, password) {
-  baseString = Base64.btoa(username + ':' + password).replace('\n', '\\n');
-  return fetch(`https://api.github.com/authorizations/clients/${consts.CLIENT_ID}`, {
-    method: 'PUT',
-    headers: {
-      ...consts.BASE_HEADER,
-      "Authorization": "Basic " + baseString
-    },
-    body: JSON.stringify({
-      client_secret: consts.CLIENT_SECRET,
+    const baseString = Base64.btoa(username + ':' + password).replace('\n', '\\n');
+    return fetch(`https://api.github.com/authorizations/clients/${consts.CLIENT_ID}`, {
+        method: 'PUT',
+        headers: {
+            ...consts.BASE_HEADER,
+            "Authorization": "Basic " + baseString
+        },
+        body: JSON.stringify({
+            client_secret: consts.CLIENT_SECRET,
+        })
+    }).then((user) => {
+        return user.json()
     })
-  }).then((user) => {
-    return user.json()
-  })
-    .then((responseJson) => {
-      return responseJson;
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+        .then((responseJson) => {
+            return responseJson;
+        })
+        .catch((error) => {
+            console.log(error);
+        });
 }
